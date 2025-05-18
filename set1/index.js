d67c5b9d9e600a792ee5d49f7c51b810fb455486
Jigarunagar <officialjigarunagar@gmail.com>
2025-05-18 11:26:51 +0530

    update

--- set1/index.js
+++ set1/index.js
@@ -1,35 +1,31 @@
-const http = require('http')
-const dotenv = require('dotenv')
+const http = require("http");
+const dotenv = require("dotenv");
 // const url = require('url')
 
-dotenv.config(
-{path:'./.env'}
-)
+dotenv.config({ path: "./.env" });
 
-const server = http.createServer((req , res) => {
+const server = http.createServer((req, res) => {
+  let url = req.url;
 
-    let url = req.url
+  if (url === "/") {
+    res.write("Home Page");
+    res.end();
+  } else if (url === "/about") {
+    res.write("About Page");
+    res.end();
+  } else if (url === "/faq") {
+    res.write("FAQ Page");
+    res.end();
+  } else {
+    res.writeHead(404);
+    res.write("404 Page Not Found!!!");
+    res.end();
+  }
+});
 
-    if(url === '/'){
-        res.write('Home Page')
-        res.end()
-    }else if(url === '/about'){
-         res.write('About Page')
-        res.end()
-    }else if(url === '/faq'){
-         res.write('FAQ Page')
-        res.end()
-    }else{
-        res.writeHead(404)
-        res.write('404 Page Not Found!!!')
-        res.end()
-    }
-})
-
-server.listen(process.env.PORT , (err) => {
-    if(err){
-        console.log('server not successfully start!!');
-    }
-
-    console.log('server successfully start!!');
-})
+server.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log("server not successfully start!!");
+  }
+  console.log("server successfully start!!");
+});
