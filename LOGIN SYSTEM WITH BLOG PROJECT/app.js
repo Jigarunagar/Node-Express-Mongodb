ad525d0606302b17e4217ed2821f879df82fec99
Jigar Unagar <officialjigarunagar@gmail.com>
2025-07-18 00:06:44 +0530

    update

--- LOGIN SYSTEM WITH BLOG PROJECT/app.js
+++ LOGIN SYSTEM WITH BLOG PROJECT/app.js
@@ -23,10 +23,7 @@ app.use("/", require("./routes/blog"));
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error(" MongoDB connection failed:", err.message);
