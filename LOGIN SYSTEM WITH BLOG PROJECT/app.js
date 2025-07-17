aa7bce62c58464652208fb446e95333f31a7c40b
Jigar Unagar <officialjigarunagar@gmail.com>
2025-07-18 00:03:08 +0530

    update

---
+++ LOGIN SYSTEM WITH BLOG PROJECT/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+require("dotenv").config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use(
+  session({
+    secret: "secretkey",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use("/auth", require("./routes/auth"));
+app.use("/", require("./routes/blog"));
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error(" MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+connectDB()
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
